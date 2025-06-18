6e9ad13d95a52bce430ea68726fc4fab0a14565a
RMohanRaj007 <mohanraj.r8007@gmail.com>
2025-06-18 19:23:06 +0530

    Add files via upload

---
+++ iterate ().py
@@ -0,0 +1,16 @@
+class EvenNumbers:
+    def __iter__(self):
+        self.n = 2  
+        return self
+
+    def __next__(self):
+        x = self.n
+        self.n += 2  
+        return x
+even = EvenNumbers()
+it = iter(even)
+print(next(it))  
+print(next(it)) 
+print(next(it))  
+print(next(it)) 
+print(next(it))
